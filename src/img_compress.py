0de6846ccdc16f05737fe7a31bf53dedf417627d
Rdyx <tira.nicolas@gmail.com>
2020-02-03 13:30:44 +0100

    Add output example as demo

--- src/img_compress.py
+++ src/img_compress.py
@@ -8,7 +8,7 @@ def get_image_compression_quality(
     compression_arg=settings.COMPRESSION_QUALITY_ARG,
     args_list_setting=settings.ARGS_LIST,
 ):
-    """ Use compression quality user wants if set from arg, default to 50 """
+    """ Use compression quality user wants if set from arg, default to 65 """
     if compression_arg:
         return int(utils.get_arg_value(compression_arg, args_list_setting))
     return 65
